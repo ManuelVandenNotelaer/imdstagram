dd1be3d1b3f617e64b9784c0c5d4584422e0720c
Van den Notelaer Manuel <manuel_vdn@hotmail.com>
2016-03-30 00:59:04 +0200

    css/style.css

--- detail.php
+++ detail.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>IMDstagram</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <div id="container">
+        <?php include_once('header.php'); ?>
+    </div><!-- end of container -->
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,7 +1,10 @@
 <header>
-    <img src="images/instagram.png" alt="">
-    <div id="header_right">
-        <input type="text" placeholder="Search">
-        <a href="#">Login</a>
+   <div id="header_container">
+        <img src="images/instagram.png" alt="">
+        <div id="header_right">
+           <div id="search">
+            <input type="text" placeholder="Search">
+            <a href="login.php">Login</a>
+        </div>
     </div>
 </header>
